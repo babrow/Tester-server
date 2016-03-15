724481255ca8f5750a18fb1f379637b1a2199e7f
babrow <lgs.babrow@gmail.com>
2016-03-16 01:14:54 +0300

    Replaced chart for tapping test

--- app/controllers/Stats.java
+++ app/controllers/Stats.java
@@ -2,7 +2,6 @@ package controllers;
 
 import models.Account;
 import models.Test;
-import models.TestResult;
 import models.TestResultAttr;
 import play.db.jpa.JPA;
 import play.mvc.Controller;
@@ -11,6 +10,7 @@ import play.mvc.With;
 
 import javax.persistence.Query;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -24,7 +24,7 @@ public class Stats extends Controller {
     public static List<String[]> getTappingData(Collection<TestResultAttr> attrs) {
         List<String[]> results = new ArrayList<>();
         for (TestResultAttr attr : attrs) {
-            results.add(new String[]{attr.getTestAttr().getName(), String.valueOf(attr.getVal())});
+            results.add(new String[]{String.valueOf(attr.getTestAttr().getSortOrder() - 1), String.valueOf(attr.getVal())});
         }
         return results;
     }
@@ -32,16 +32,17 @@ public class Stats extends Controller {
     public static void index() {
         Test test = Test.findById(1L);
         Account account = Account.find("byEmail", Security.connected()).first();
-        List<TestResult> testResults = TestResult.find("byTestAndAccountAndResultIsNotNull", test, account).fetch();
 
-        TestResultAttr firstRes = TestResultAttr.find("testResult.test = ? and testResult.account = ? order by testResult.resultDate", test, account).first();
+        TestResultAttr lastRes = TestResultAttr.find("testResult.test = ? and testResult.account = ? order by testResult.resultDate", test, account).first();
+        List<String[]> last = getTappingData(Arrays.asList(lastRes));
         List<TestResultAttr> allRes = TestResultAttr.find("testResult.test = ? and testResult.account = ?", test, account).fetch();
-        @SuppressWarnings("JpaQlInspection") Query query = JPA.em().createQuery("select avg(a.val), t.name from TestResult r, TestResultAttr a, TestAttr t where a.testResult = r and a.testAttr = t and r.test = :test and r.account = :account group by t.name");
+        List<String[]> all = getTappingData(allRes);
+        @SuppressWarnings("JpaQlInspection") Query query = JPA.em().createQuery("select t.sortOrder - 1, avg(a.val) from TestResult r, TestResultAttr a, TestAttr t where a.testResult = r and a.testAttr = t and r.test = :test and r.account = :account group by t.sortOrder");
         query.setParameter("test", test);
         query.setParameter("account", account);
-        List resultList = query.getResultList();
+        List<String[]> avg = query.getResultList();
 
 
-        render(account, testResults);
+        render(account, last, avg, all);
     }
 }
