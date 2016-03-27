f94312d9f8b325ed903d81e99fb51eac6a65e3ae
babrow <lgs.babrow@gmail.com>
2016-03-27 13:21:36 +0300

    Avg  and last results are line type now

--- app/controllers/Stats.java
+++ app/controllers/Stats.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.Account;
 import models.Test;
+import models.TestResult;
 import models.TestResultAttr;
 import play.db.jpa.JPA;
 import play.mvc.Controller;
@@ -10,7 +11,6 @@ import play.mvc.With;
 
 import javax.persistence.Query;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -32,12 +32,15 @@ public class Stats extends Controller {
     public static void index() {
         Test test = Test.findById(1L);
         Account account = Account.find("byEmail", Security.connected()).first();
+        TestResult testResult = TestResult.find("test = ? and account = ? order by resultDate desc", test, account).first();
 
-        TestResultAttr lastRes = TestResultAttr.find("testResult.test = ? and testResult.account = ? order by testResult.resultDate desc", test, account).first();
-        List<String[]> last = getTappingData(Arrays.asList(lastRes));
+        List<TestResultAttr> lastRes = TestResultAttr.find("byTestResult", testResult).fetch();
+        List<String[]> last = getTappingData(lastRes);
         List<TestResultAttr> allRes = TestResultAttr.find("testResult.test = ? and testResult.account = ?", test, account).fetch();
         List<String[]> all = getTappingData(allRes);
-        @SuppressWarnings("JpaQlInspection") Query query = JPA.em().createQuery("select t.sortOrder - 1, avg(a.val) from TestResult r, TestResultAttr a, TestAttr t where a.testResult = r and a.testAttr = t and r.test = :test and r.account = :account group by t.sortOrder");
+        @SuppressWarnings("JpaQlInspection") Query query = JPA.em().createQuery("select t.sortOrder - 1, avg(a.val) " +
+                "from TestResult r, TestResultAttr a, TestAttr t " +
+                "where a.testResult = r and a.testAttr = t and r.test = :test and r.account = :account group by t.sortOrder order by t.sortOrder");
         query.setParameter("test", test);
         query.setParameter("account", account);
         List<String[]> avg = query.getResultList();
